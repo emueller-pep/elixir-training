83de5a6d5e080468030032e9dbb9cdfe9e4dabaf
Eric Mueller <nevinera@gmail.com>
2019-11-18 20:35:14 -0500

    exercises 2.1-2.3 (2.3 is untested, not sure how to test side-effects yet)

---
+++ sequential_test.exs
@@ -0,0 +1,34 @@
+ExUnit.start()
+
+defmodule SequentialTest do
+  use ExUnit.Case, async: true
+
+  test "Sequential.sum/1" do
+    assert Sequential.sum(1) == 1
+    assert Sequential.sum(5) == 15
+    assert Sequential.sum(10) == 55
+    catch_error Sequential.sum(-3)
+  end
+
+  test "Sequential.sum_interval/2" do
+    assert Sequential.sum_interval(1, 5) == 15
+    assert Sequential.sum_interval(3, 5) == 12
+    assert Sequential.sum_interval(-4, -3) == -7
+    assert Sequential.sum_interval(-1, 1) == 0
+    catch_error Sequential.sum_interval(6, 5)
+  end
+
+  test "Sequence.create/1" do
+    assert Sequential.create(1) == [1]
+    assert Sequential.create(4) == [1, 2, 3, 4]
+    catch_error Sequential.create(-2)
+    catch_error Sequential.create(0)
+  end
+
+  test "Sequence.reverse_create/1" do
+    assert Sequential.reverse_create(1) == [1]
+    assert Sequential.reverse_create(4) == [4, 3, 2, 1]
+    catch_error Sequential.reverse_create(-2)
+    catch_error Sequential.reverse_create(0)
+  end
+end
