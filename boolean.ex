c57308e7627f142ab8f528cf68ceede0e4a8f582
Eric Mueller <nevinera@gmail.com>
2019-11-18 20:37:41 -0500

    exercise 1.5, boolean via pattern-matching

---
+++ boolean.ex
@@ -0,0 +1,17 @@
+defmodule Boolean do
+  @moduledoc """
+  Reimplement simple boolean logic
+  """
+
+  @doc "strict reimplementation of a boolean NOT"
+  def b_not(true) do false end
+  def b_not(false) do true end
+
+  @doc "strict reimplementation of a boolean AND"
+  def b_and(true, true) do true end
+  def b_and(_, _) do false end
+
+  @doc "strict reimplementation of a boolean OR"
+  def b_or(false, false) do false end
+  def b_or(_, _) do true end
+end
---
+++ boolean_test.exs
@@ -0,0 +1,24 @@
+ExUnit.start()
+
+defmodule BooleanTest do
+  use ExUnit.Case, async: true
+
+  test "Boolean.b_not" do
+    assert Boolean.b_not(true) == false
+    assert Boolean.b_not(false) == true
+  end
+
+  test "Boolean.b_and" do
+    assert Boolean.b_and(true, true) == true
+    assert Boolean.b_and(true, false) == false
+    assert Boolean.b_and(false, true) == false
+    assert Boolean.b_and(false, false) == false
+  end
+
+  test "Boolean.b_or" do
+    assert Boolean.b_or(true, true) == true
+    assert Boolean.b_or(true, false) == true
+    assert Boolean.b_or(false, true) == true
+    assert Boolean.b_or(false, false) == false
+  end
+end
