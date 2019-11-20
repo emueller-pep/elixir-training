5c893d6d6bb2a550395f5bae8b377fda48ccece6
Eric Mueller <eric.mueller@pepsico.com>
2019-11-20 11:04:55 -0500

    implement MapDb with tests (exercise 6.1)

---
+++ lib/map_db.ex
@@ -0,0 +1,35 @@
+defmodule MapDb do
+  @moduledoc """
+  Take the db.ex module you wrote in exercise 2.4. Rewrite it using maps. Test it using your
+  database server you wrote in exercise 4.1.
+
+  Note: Make sure you save a copy of your db.ex module using lists somewhere else (or with a
+  new name) before you start changing in.
+  """
+
+  @doc "create a new database"
+  def new do %{} end
+
+  @doc "clean up an existing database"
+  def destroy(_db) do :ok end
+
+  @doc "write a new record onto the database"
+  def write(db, key, value) do Map.put(db, key, value) end
+
+  @doc "delete a record from the database"
+  def delete(db, key) do Map.delete(db, key) end
+
+  @doc "read the value for a record from the database"
+  def read(db, key) do
+    case db do
+      %{^key => value} -> { :ok, value }
+      _ -> { :error, :instance }
+    end
+  end
+
+  @doc "find all keys having the supplied value in the database"
+  def match(db, value) do
+    Enum.filter(db, fn({k,v}) -> v == value end)
+    |> Enum.map(fn({k,_v}) -> k end)
+  end
+end
---
+++ test/map_db_test.exs
@@ -0,0 +1,42 @@
+defmodule MapDbTest do
+  use ExUnit.Case, async: true
+  doctest MapDb
+
+  test "MapDb.new/0" do
+    assert MapDb.new == %{}
+  end
+
+  test "MapDb.destroy/1" do
+    db = MapDb.new
+    assert MapDb.destroy(db) == :ok
+  end
+
+  test "MapDb.write/3" do
+    db = MapDb.new
+         |> MapDb.write(:foo, :bar)
+         |> MapDb.write(:baz, 3)
+    assert db == %{foo: :bar, baz: 3}
+  end
+
+  test "MapDb.delete/2" do
+    db = %{baz: 3, foo: :bar} |> MapDb.delete(:foo)
+    assert db == %{baz: 3}
+
+    db = %{baz: 3, foo: :bar} |> MapDb.delete(:baz)
+    assert db == %{foo: :bar}
+  end
+
+  test "MapDb.read/2" do
+    db = %{baz: 3, foo: :bar}
+    assert MapDb.read(db, :baz) == {:ok, 3}
+    assert MapDb.read(db, :foo) == {:ok, :bar}
+    assert MapDb.read(db, :bar) == {:error, :instance}
+  end
+
+  test "MapDb.match/2" do
+    db = %{foo: 1, bar: 2, baz: 3, bim: 2}
+    assert MapDb.match(db, 1) == [:foo]
+    assert MapDb.match(db, 3) == [:baz]
+    assert MapDb.match(db, 2) == [:bar, :bim]
+  end
+end
